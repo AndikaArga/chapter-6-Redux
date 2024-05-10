0a9c4f045700bee8393dac244c6cc096f25da641
Andika Arga Pradana <andikaarga2002@gmail.com>
2024-05-10 20:36:26 +0700

    ubah

--- src/userPage.jsx
+++ src/userPage.jsx
@@ -5,10 +5,10 @@ import Gamelist from "./Gamelist";
 export default function UserPage() {
   return (
     <div className="w-screen h-screen bg-cover bg-[#222222] overflow-y-auto">
-      <div>
+      <div className="fixed top-0 w-full z-50">
         <Navbar />
       </div>
-      <div className="mx-52 mt-4">
+      <div className=" mt-20">
         <Gamelist />
       </div>
     </div>
